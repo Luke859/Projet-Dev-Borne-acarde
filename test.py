5b1b581e9bc95f67eb7535f29d496e0de44e297b
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-04-28 11:33:54 +0200

    Fonctionnalités - Affichage d'une page + color fond ecran + ajout de 2 images sur page

---
+++ test.py
@@ -0,0 +1,47 @@
+import pygame
+import os 
+
+pygame.init()
+
+display = pygame.display.set_mode((1000, 600))
+pygame.display.set_caption("Borne d'arcade")
+RED = (255, 0, 0)
+FPS = 60 
+CHARACTER_WIDTH, CHARACTER_HEIGHT =  120, 100
+
+#CHARACTER 1
+CHARACTER1_IMAGE = pygame.image.load(os.path.join('images', 'ninja.png'))
+CHARACTER1 = pygame.transform.scale(CHARACTER1_IMAGE, (CHARACTER_WIDTH, CHARACTER_HEIGHT))
+
+#CHARACTER 2
+CHARACTER2_IMAGE = pygame.image.load(os.path.join('images', 'pacman.png'))
+CHARACTER2 = pygame.transform.scale(CHARACTER2_IMAGE, (CHARACTER_WIDTH, CHARACTER_HEIGHT))
+
+#Style d notre jeu
+def window_style(character1, character2):
+    display.fill(RED)
+    display.blit(CHARACTER1, (character1.x, character1.y)) 
+    display.blit(CHARACTER2, (character2.x, character2.y)) 
+    pygame.display.update()
+
+#Page de notre jeu
+def main():
+    character1 = pygame.Rect(100, 300, CHARACTER_WIDTH, CHARACTER_HEIGHT)
+    character2 = pygame.Rect(700, 300, CHARACTER_WIDTH, CHARACTER_HEIGHT)
+
+    clock = pygame.time.Clock()  #Controller la vitesse d'affichage de notre boucle 
+    open = True
+    while open:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                open = False
+
+        character1.x += 1
+        character2.x -= 1
+
+        window_style(character1, character2)
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
