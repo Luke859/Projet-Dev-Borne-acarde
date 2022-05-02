87a7b13efd63555c7556891fe0549faedea46fad
nathymellal <72868466+nathymellal@users.noreply.github.com>
2022-05-02 15:01:22 +0200

    add new player

--- game.py
+++ game.py
@@ -2,5 +2,7 @@ from player import Player
 
 class Game:
     def __init__(self):
-        self.player = Player()
+        self.player = Player("assets/red_tank.png", 100, 100)
+        self.player2 = Player("assets/blue_tank.png", 800, 600)
         self.pressed = {}
+
--- player.py
+++ player.py
@@ -4,19 +4,19 @@ from projectile import Projectile
 class Player(pygame.sprite.Sprite):
 
 
-    def __init__(self):
+    def __init__(self, sprite, x, y):
         super().__init__()
         self.health = 100
         self.max_health = 100
         self.attack = 10
         self.velocity = 7
         self.all_projectiles = pygame.sprite.Group()
-        self.image = pygame.image.load("assets/red_tank.png")
+        self.image = pygame.image.load(sprite)
         self.image = pygame.transform.rotate(self.image, 270)
         self.image = pygame.transform.scale(self.image, (100,100))
         self.rect = self.image.get_rect()
-        self.rect.x = 400   
-        self.rect.y = 300
+        self.rect.x = x
+        self.rect.y = y
     
     def launch_projectile(self):
         self.all_projectiles.add(Projectile(self))
