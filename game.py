47d64a37779160a668db70cf68dc95bbbddbb8ea
nathymellal <72868466+nathymellal@users.noreply.github.com>
2022-05-02 15:51:37 +0200

    report failed

--- game.py
+++ game.py
@@ -3,6 +3,6 @@ from player import Player
 class Game:
     def __init__(self):
         self.player = Player("assets/red_tank.png", 100, 100)
-        self.player2 = Player("assets/blue_tank.png", 800, 600)
+        self.player2 = Player("assets/bleu_tank.png", 200, 200)
         self.pressed = {}
 
