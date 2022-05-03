2dce345296dfa6840c39366cc3fd42554fe33cd7
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-05-03 13:59:13 +0200

    image

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ screen = pygame.display.set_mode((800,400))
 backgroound = pygame.image.load("assets/Clipboard01.jpg")
 
 banner = pygame.image.load("assets/tankImage.png")
-banner = pygame.transform.scale(banner, (100, 100))
+banner = pygame.transform.scale(banner, (250, 250))
 banner_rect = banner.get_rect()
 banner_rect.x = screen.get_width() / 2 
 
