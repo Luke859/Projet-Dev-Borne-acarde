ec930945adaa3e64449edb67e96f5a78d7798f9d
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-05-06 16:12:47 +0200

    accueil

--- main.py
+++ main.py
@@ -2,47 +2,54 @@ import pygame
 import math
 from game import Game
 from gpiozero import Button
-pygame.init()
 
-pygame.display.set_caption("Test")
-screen = pygame.display.set_mode((800,400))
+# pygame.init()
+
+# pygame.display.set_caption("Test")
+# screen = pygame.display.set_mode((800,400))
 
 background = pygame.image.load("assets/Clipboard01.jpg")
 
+game = Game()
+
+while game.running:
+    game.curr_menu.display_menu()
+    game.game_loop()
+
 # button1blue = Button("GPIO12")
 
-banner = pygame.image.load("assets/tankImage.png")
-banner = pygame.transform.scale(banner, (200, 200))
-banner_rect = banner.get_rect()
-banner_rect.x = math.ceil(screen.get_width() / 3)
+# banner = pygame.image.load("assets/tankImage.png")
+# banner = pygame.transform.scale(banner, (200, 200))
+# banner_rect = banner.get_rect()
+# banner_rect.x = math.ceil(screen.get_width() / 3)
 
-play_button = pygame.image.load('assets/button.png')
-play_button = pygame.transform.scale(play_button, (400, 150))
-play_button_rect = play_button.get_rect()
-play_button_rect.x = math.ceil(screen.get_width() / 3.33)
-play_button_rect.y = math.ceil(screen.get_height() / 2)
+# play_button = pygame.image.load('assets/button.png')
+# play_button = pygame.transform.scale(play_button, (400, 150))
+# play_button_rect = play_button.get_rect()
+# play_button_rect.x = math.ceil(screen.get_width() / 3.33)
+# play_button_rect.y = math.ceil(screen.get_height() / 2)
 
-game = Game()
-running = True
+# game = Game()
+# running = True
 
-while running:
+# while running:
 
-    screen.blit(background,(0,0))
+#     screen.blit(background,(0,0))
 
-    if game.is_playing:
-        game.update(screen)
+#     if game.is_playing:
+#         game.update(screen)
 
-    else:
-        screen.blit(banner, banner_rect)
-        screen.blit(play_button, play_button_rect)
+#     else:
+#         screen.blit(banner, banner_rect)
+#         screen.blit(play_button, play_button_rect)
 
 
-    pygame.display.flip()
+#     pygame.display.flip()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            pygame.quit()
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running = False
+#             pygame.quit()
             
         # if event.type == button1blue.is_pressed :
         #     if play_button_rect.collidepoint(event.pos):
