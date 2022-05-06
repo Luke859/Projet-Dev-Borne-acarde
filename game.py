4b6893c55e48a38403dfce65984be5e15cd86f98
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-05-06 16:34:19 +0200

    accueil

--- game.py
+++ game.py
@@ -1,6 +1,22 @@
 import pygame
 from menu import *
+from player import Player
+from gpiozero import Button
 
+button1red = Button("GPIO15")
+button2red = Button("GPIO18")
+button1blue = Button("GPIO12")
+button2blue = Button("GPIO07")
+
+joystickBlueUp = Button("GPIO11")
+joystickBlueLeft =Button("GPIO06")
+joystickBlueRight = Button("GPIO13")
+joystickBlueDown = Button("GPIO05")
+
+joystickRedUp = Button("GPIO04")
+joystickRedLeft =Button("GPIO27")
+joystickRedRight = Button("GPIO22")
+joystickRedDown = Button("GPIO17")
 
 class Game():
     def __init__(self):
@@ -8,10 +24,9 @@ class Game():
         self.running, self.playing = True, False
         self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
         self.DISPLAY_W, self.DISPLAY_H = 480, 270
-        self.display = pygame.Surface((self.DISPLAY_W,self.DISPLAY_H))
+        self.background = pygame.image.load("assets/Clipboard01.jpg")
         self.window = pygame.display.set_mode(((self.DISPLAY_W,self.DISPLAY_H)))
         self.font_name = '8-BIT WONDER.TTF'
-        #self.font_name = pygame.font.get_default_font()
         self.BLACK, self.WHITE = (0, 0, 0), (255, 255, 255)
         self.main_menu = MainMenu(self)
         self.options = OptionsMenu(self)
@@ -24,8 +39,8 @@ class Game():
             if self.START_KEY:
                 self.playing= False
             self.display.fill(self.BLACK)
-            self.draw_text('Thanks for Playing', 20, self.DISPLAY_W/2, self.DISPLAY_H/2)
-            self.window.blit(self.display, (0,0))
+            self.window.blit(self.background, (0,0))
+            self.window.blit(self.player.image, self.player.rect)
             pygame.display.update()
             self.reset_keys()
 
@@ -55,24 +70,6 @@ class Game():
         text_rect = text_surface.get_rect()
         text_rect.center = (x,y)
         self.display.blit(text_surface,text_rect)
-        
-# from player import Player
-# from gpiozero import Button
-
-# button1red = Button("GPIO15")
-# button2red = Button("GPIO18")
-# button1blue = Button("GPIO12")
-# button2blue = Button("GPIO07")
-
-# joystickBlueUp = Button("GPIO11")
-# joystickBlueLeft =Button("GPIO06")
-# joystickBlueRight = Button("GPIO13")
-# joystickBlueDown = Button("GPIO05")
-
-# joystickRedUp = Button("GPIO04")
-# joystickRedLeft =Button("GPIO27")
-# joystickRedRight = Button("GPIO22")
-# joystickRedDown = Button("GPIO17")
 
     # def update(self, screen): 
     #     screen.blit(self.player.image, self.player.rect)
