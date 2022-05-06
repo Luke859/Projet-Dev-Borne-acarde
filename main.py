3f25dd873d4a0e38c014d751c5e931226868382c
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-05-06 18:47:57 +0200

    test luke page accueil fonctionnelle

--- main.py
+++ main.py
@@ -9,22 +9,6 @@ while running:
     
     game.curr_menu.display_menu()
     game.game_loop()
-        
-    print(game.player.rect.x)
     
-
     pygame.display.flip()
-
-    # for event in pygame.event.get():
-    #     if event.type == pygame.QUIT:
-    #         running = False
-    #         pygame.quit()
-    #     elif event.type == pygame.KEYDOWN:
-    #         game.pressed[event.key] = True
-
-    #         if button2blue.is_pressed :
-    #             game.player.launch_projectile()
-
-    #     elif event.type == pygame.KEYUP:
-    #         game.pressed[event.key] = False
           
