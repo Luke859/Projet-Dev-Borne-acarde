358f8273f140769e4a607080417377080b75194a
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-05-06 17:49:58 +0200

    accueil

--- game.py
+++ game.py
@@ -1,5 +1,3 @@
-import pygame
-from menu import *
 from player import Player
 from gpiozero import Button
 
@@ -18,76 +16,28 @@ joystickRedLeft =Button("GPIO27")
 joystickRedRight = Button("GPIO22")
 joystickRedDown = Button("GPIO17")
 
-class Game():
+class Game:
+
     def __init__(self):
-        pygame.init()
-        self.running, self.playing = True, False
-        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
-        self.DISPLAY_W, self.DISPLAY_H = 800,400
-        self.display = pygame.Surface((self.DISPLAY_W,self.DISPLAY_H))
-        self.background = pygame.image.load("assets/Clipboard01.jpg")
-        self.window = pygame.display.set_mode(((self.DISPLAY_W,self.DISPLAY_H)))
-        self.font_name = '8-BIT WONDER.TTF'
-        self.BLACK, self.WHITE = (0, 0, 0), (255, 255, 255)
-        self.main_menu = MainMenu(self)
-        self.options = OptionsMenu(self)
-        self.credits = CreditsMenu(self)
-        self.curr_menu = self.main_menu
+        self.is_playing = False
+        self.player = Player()
+        self.pressed = {}
 
-    def game_loop(self):
-        while self.playing:
-            self.check_events()
-            if self.START_KEY:
-                self.playing= False
-            # self.window.blit(self.player.image, self.player.rect)
-            self.window.blit(self.background, (0,0))
-            self.draw_text('Thanks for Playing', 20, self.DISPLAY_W/2, self.DISPLAY_H/2)
-            pygame.display.update()
-            self.reset_keys()
+    def update(self, screen): 
+        screen.blit(self.player.image, self.player.rect)
 
+        for projectile in self.player.all_projectiles:
+            projectile.move()
 
+        self.player.all_projectiles.draw(screen)
 
-    def check_events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.running, self.playing = False, False
-                self.curr_menu.run_display = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    self.START_KEY = True
-                if event.key == pygame.K_BACKSPACE:
-                    self.BACK_KEY = True
-                if event.key == pygame.K_DOWN:
-                    self.DOWN_KEY = True
-                if event.key == pygame.K_UP:
-                    self.UP_KEY = True
-
-    def reset_keys(self):
-        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
-
-    def draw_text(self, text, size, x, y ):
-        font = pygame.font.Font(self.font_name,size)
-        text_surface = font.render(text, True, self.WHITE)
-        text_rect = text_surface.get_rect()
-        text_rect.center = (x,y)
-        self.display.blit(text_surface,text_rect)
-
-    # def update(self, screen): 
-    #     screen.blit(self.player.image, self.player.rect)
-
-    #     for projectile in self.player.all_projectiles:
-    #         projectile.move()
-
-    #     self.player.all_projectiles.draw(screen)
-
-    #     if joystickBlueRight.is_pressed and self.player.rect.x < screen.get_width() - game.player.rect.width:
-    #         self.player.move_right()
-    #     elif joystickBlueLeft.is_pressed and self.player.rect.x > 0:
-    #         self.player.move_left()
-    #     elif joystickBlueUp.is_pressed and self.player.rect.y > 0:
-    #         self.player.move_up()
-    #     elif joystickBlueDown.is_pressed and self.player.rect.y < screen.get_height() - game.player.rect.height:
-    #         self.player.move_down()
-    #     if button2blue.is_pressed :
-    #         self.player.launch_projectile()
-
+        if joystickBlueRight.is_pressed and self.player.rect.x < screen.get_width() - game.player.rect.width:
+            self.player.move_right()
+        elif joystickBlueLeft.is_pressed and self.player.rect.x > 0:
+            self.player.move_left()
+        elif joystickBlueUp.is_pressed and self.player.rect.y > 0:
+            self.player.move_up()
+        elif joystickBlueDown.is_pressed and self.player.rect.y < screen.get_height() - game.player.rect.height:
+            self.player.move_down()
+        if button2blue.is_pressed :
+            self.player.launch_projectile()
--- main.py
+++ main.py
@@ -1,57 +1,67 @@
+import pygame 
+import math
 from game import Game
+from gpiozero import Button
+pygame.init()
 
-g = Game()
+pygame.display.set_caption("Test")
+screen = pygame.display.set_mode((800,400))
 
-while g.running:
-    g.curr_menu.display_menu()
-    g.game_loop()
+backgroound = pygame.image.load("assets/Clipboard01.jpg")
 
-# import pygame 
-# import math
-# from gpiozero import Button
+button1blue = Button("GPIO12")
 
-# pygame.init()
+banner = pygame.image.load("assets/tankImage.png")
+banner = pygame.transform.scale(banner, (200, 200))
+banner_rect = banner.get_rect()
+banner_rect.x = math.ceil(screen.get_width() / 3)
 
-# pygame.display.set_caption("Test")
-# screen = pygame.display.set_mode((800,400))
+play_button = pygame.image.load('assets/button.png')
+play_button = pygame.transform.scale(play_button, (400, 150))
+play_button_rect = play_button.get_rect()
+play_button_rect.x = math.ceil(screen.get_width() / 3.33)
+play_button_rect.y = math.ceil(screen.get_height() / 2)
 
-# background = pygame.image.load("assets/Clipboard01.jpg")
+game = Game()
+running = True
 
-# button1blue = Button("GPIO12")
+# def ButtonOn(button):
+#     print("button " + str(button.pin) + " pressed")
 
-# banner = pygame.image.load("assets/tankImage.png")
-# banner = pygame.transform.scale(banner, (200, 200))
-# banner_rect = banner.get_rect()
-# banner_rect.x = math.ceil(screen.get_width() / 3)
+# def ButtonOff(button):
+#     print("button " + str(button.pin) + " OFF")
+    
+# def JoystickUp(joystick):
+#     print("Joystick UP " + str(joystick.pin))
+    
+# def JoystickLeft(joystick):
+#     print("Joystick LEFT " + str(joystick.pin))
+    
+# def JoystickRight(joystick):
+#     print("Joystick RIGHT " + str(joystick.pin))
+    
+# def JoystickDown(joystick):
+#     print("Joystick DOWN " + str(joystick.pin))
 
-# play_button = pygame.image.load('assets/button.png')
-# play_button = pygame.transform.scale(play_button, (400, 150))
-# play_button_rect = play_button.get_rect()
-# play_button_rect.x = math.ceil(screen.get_width() / 3.33)
-# play_button_rect.y = math.ceil(screen.get_height() / 2)
+while running:
 
-# game = Game()
-# running = True
+    screen.blit(backgroound,(0,0))
 
-# while running:
+    if game.is_playing:
+        game.update(screen)
 
-#     screen.blit(background,(0,0))
-
-#     if game.is_playing:
-#         game.update(screen)
-
-#     else:
-#         screen.blit(banner, banner_rect)
-#         screen.blit(play_button, play_button_rect)
+    else:
+        screen.blit(banner, banner_rect)
+        screen.blit(play_button, play_button_rect)
 
 
-#     pygame.display.flip()
+    pygame.display.flip()
 
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             running = False
-#             pygame.quit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
             
-        # if event.type == button1blue.is_pressed :
-        #     if play_button_rect.collidepoint(event.pos):
-        #         game.is_playing = True 
+        if event.type == button1blue.is_pressed :
+            if play_button_rect.collidepoint(event.pos):
+                game.is_playing = True 
