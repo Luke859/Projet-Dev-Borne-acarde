f9a566746b254f20eccff27056aa30fbe135a8b6
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-05-06 18:00:54 +0200

    accueil

--- main.py
+++ main.py
@@ -9,6 +9,21 @@ screen = pygame.display.set_mode((800,400))
 
 backgroound = pygame.image.load("assets/Clipboard01.jpg")
 
+button1red = Button("GPIO15")
+button2red = Button("GPIO18")
+button1blue = Button("GPIO12")
+button2blue = Button("GPIO07")
+
+joystickBlueUp = Button("GPIO11")
+joystickBlueLeft =Button("GPIO06")
+joystickBlueRight = Button("GPIO13")
+joystickBlueDown = Button("GPIO05")
+
+joystickRedUp = Button("GPIO04")
+joystickRedLeft =Button("GPIO27")
+joystickRedRight = Button("GPIO22")
+joystickRedDown = Button("GPIO17")
+
 # button1blue = Button("GPIO12")
 
 banner = pygame.image.load("assets/tankImage.png")
@@ -32,11 +47,47 @@ while running:
 
     if game.is_playing:
         game.update(screen)
+        screen.blit(game.player.image, game.player.rect)
+        screen.blit(game.player2.image, game.player2.rect)
+
+        for projectile in game.player.all_projectiles:
+            projectile.move(1)
+
+        game.player.all_projectiles.draw(screen)
+
+        if joystickBlueRight.is_pressed and game.player.rect.x < screen.get_width() - game.player.rect.width:
+            game.player.move_right()
+        elif joystickBlueLeft.is_pressed and game.player.rect.x > 0:
+            game.player.move_left()
+        elif joystickBlueUp.is_pressed and game.player.rect.y > 0:
+            game.player.move_up()
+        elif joystickBlueDown.is_pressed and game.player.rect.y < screen.get_height() - game.player.rect.height:
+            game.player.move_down()
+        if button2blue.is_pressed :
+            game.player.launch_projectile()
+
+        
+        for projectile in game.player2.all_projectiles:
+            projectile.move(-1)
+
+            game.player2.all_projectiles.draw(screen)
+
+        if joystickRedRight.is_pressed and game.player2.rect.x < screen.get_width() - game.player2.rect.width:
+            game.player2.move_right()
+        elif joystickRedLeft.is_pressed and game.player2.rect.x > 0:
+            game.player2.move_left()
+        elif joystickRedUp.is_pressed and game.player2.rect.y > 0:
+            game.player2.move_up()
+        elif joystickRedDown.is_pressed and game.player2.rect.y < screen.get_height() - game.player2.rect.height:
+            game.player2.move_down()
+        if button2red.is_pressed :
+            game.player2.launch_projectile()
 
     else:
         # screen.blit(banner, banner_rect)
         # screen.blit(play_button, play_button_rect)
         game.update(screen)
+
 
 
     pygame.display.flip()
