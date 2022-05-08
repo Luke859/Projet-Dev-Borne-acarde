72648237711db2c0b25e8b9be51d0480b4347262
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-05-08 22:46:13 +0200

    .

--- projectile.py
+++ projectile.py
@@ -8,9 +8,9 @@ class Projectile(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, ((50,50)))
         self.rect = self.image.get_rect()
 
-        self.rect.x = player.rect.x - 50
+        self.rect.x = player.rect.x
         
-        self.rect.y = player.rect.y 
+        self.rect.y = player.rect.y - 5
         self.velocity = 6
         
 
