560b9f3690da0d06996f956b383811af6004beed
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-05-08 17:29:41 +0200

    AJOUT D4UNE NOUVELLE PAGE DANS LE MENU

--- menu.py
+++ menu.py
@@ -154,6 +154,7 @@ class CreditsMenu(Menu):
 class HTPMenu(Menu):
     def __init__(self, game):
         Menu.__init__(self, game)
+        self.imageJoystick = pygame.image.load("assets/joystick.jpg")
     def display_menu(self):
         self.run_display = True
         while self.run_display:
@@ -161,6 +162,8 @@ class HTPMenu(Menu):
             if self.game.START_KEY or self.game.BACK_KEY:
                 self.game.curr_menu = self.game.main_menu
                 self.run_display = False
+            
+            self.imageJoystick.blit(self.imageJoystick,(0,0))
             self.game.display.fill(self.game.BLACK)
             self.game.draw_text('how to play', 30, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 - 50)
             self.blit_screen()
