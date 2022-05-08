6484383cacc312cadfcb3eef919f8cd0af11510a
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-05-08 22:47:12 +0200

    .

--- projectile.py
+++ projectile.py
@@ -10,7 +10,7 @@ class Projectile(pygame.sprite.Sprite):
 
         self.rect.x = player.rect.x
         
-        self.rect.y = player.rect.y - 50
+        self.rect.y = player.rect.y + 50
         self.velocity = 6
         
 
