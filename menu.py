dd50df34fe417572d6343097e1a2948dc3fc5127
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-05-08 12:53:35 +0200

    modification de la page credits

--- menu.py
+++ menu.py
@@ -5,11 +5,11 @@ class Menu():
         self.game = game
         self.mid_w, self.mid_h = self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2
         self.run_display = True
-        self.cursor_rect = pygame.Rect(0, 0, 20, 20)
+        self.cursor_rect = pygame.Rect(0, 0, 0, 20)
         self.offset = - 100
 
     def draw_cursor(self):
-        self.game.draw_text('*', 15, self.cursor_rect.x, self.cursor_rect.y)
+        self.game.draw_text('*', 25, self.cursor_rect.x, self.cursor_rect.y)
 
     def blit_screen(self):
         self.game.window.blit(self.game.display, (0, 0))
